ccac451fdee1d19dda7313af800705b4130e898e
Ojukwu1 <37628859+Ojukwu1@users.noreply.github.com>
2018-03-22 07:40:25 +0000

    Add files via upload

---
+++ aClimb.js
@@ -0,0 +1,19 @@
+// Take a climb challenge
+
+
+$(Document).ready(function (s) {
+    
+
+var contact = {
+fName: '', 
+lName: '', 
+cName: '', 
+address: '', 
+telNum: '', 
+email: '',
+age:  
+};
+
+
+
+})
