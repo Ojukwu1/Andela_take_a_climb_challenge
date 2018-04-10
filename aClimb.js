276f14516edfe44c95a43da8759b82f9d147480b
Ojukwu1 <37628859+Ojukwu1@users.noreply.github.com>
2018-04-10 17:29:33 +0100

    Add files via upload

--- aClimb.js
+++ aClimb.js
@@ -1,40 +1,14 @@
-// Take a climb challenge
+document.addEventListener("DOMContentLoaded", function (event) {
+    
 
-$(Document).ready(function (s) {
+      
+      
+     
 
-var contact = {
-fName: '', 
-lName: '', 
-cName: '', 
-address: '', 
-telNum: '', 
-email: '',
-age: 0  
-} 
-
-// Hide some boxes
-$(".hide").hide();
-
-var contact = Array();
-var name;
-name = prompt("What is your name?")
-
-function addContact(params) {
-    addC = true;
-    while (addC) {
-     var fName = new contact();   
-
-        fName.fName = fname;
-        fName.lName = lName;
-        fName.Age= Age;
-        fName.address = Address;
-        fName.telNum = TelNO;
-        fName.emailAddr = emailAddr;
-    }
-    if (fname==="") {addC = false;}
-       }
-}
+            // 
 
 
+         
+});  
+   
 
-});
